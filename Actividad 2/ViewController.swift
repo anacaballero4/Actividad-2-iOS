f48d7d9f6b4bde008f0846f4fd366b996ed85273
anacaballero4 <al02884309@tecmilenio.mx>
2024-01-31 17:27:17 -0600

    Initial Commit

---
+++ Actividad 2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Actividad 2
+//
+//  Created by Ana Sofía  on 31/01/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
