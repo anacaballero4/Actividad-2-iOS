ef889c00fcb6998a9de94b42db693dd2083b7b03
anacaballero4 <al02884309@tecmilenio.mx>
2024-01-31 19:42:39 -0600

    actualizacion

--- Actividad 2/ViewController.swift
+++ Actividad 2/ViewController.swift
@@ -8,12 +8,27 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var Bienvenidos: UILabel!
+    @IBOutlet weak var Questionmark: UILabel!
+    @IBOutlet weak var Textfield: UITextField!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
-}
+    
+    @IBAction func GuessNumber(_ sender: UIButton) {
+        let randomNumber = Int(arc4random_uniform(6)+1)
+        Questionmark.text = String(randomNumber)
+        
+        if  Textfield.text == Questionmark.text{
+            view.backgroundColor = UIColor.green
+                Bienvenidos.text = "Es correcto"
+                
+            } else{
+                view.backgroundColor = UIColor.red
+                Bienvenidos.text = "Vuelve a intentarlo"
+            }
+        }
+    }
 
